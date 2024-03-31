0877ae1916cfab04eecb05dede37ecc53f434493
Bekbolat Abdulayev <souta-azuma@proton.me>
2024-03-31 08:46:46 +0500

    chore(bin): use `NestFactory.createApplicationContext` in typeorm-datasource.js script

--- bin/typeorm-datasource.js
+++ bin/typeorm-datasource.js
@@ -4,13 +4,15 @@ const { getDataSourceToken } = require('@nestjs/typeorm')
 const { AppModule } = require('../dist/app.module')
 
 async function getDataSource() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.createApplicationContext(AppModule, {
     logger: false
   })
 
   /** @type {import('typeorm').DataSource} */
   const dataSource = app.get(getDataSourceToken())
   await dataSource.destroy()
+
+  process.on('beforeExit', () => app.close())
 
   return dataSource
 }
